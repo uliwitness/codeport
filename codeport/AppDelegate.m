c1f661b8a0491739b1c81d20c0b4490de07fa6ae
Uli Kusterer <ulivc@zathras.de>
2017-05-13 14:42:08 +0200

    Initial Commit

---
+++ codeport/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  codeport
+//
+//  Created by Uli Kusterer on 13.05.17.
+//  Copyright © 2017 Uli Kusterer. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+@end
---
+++ codeport/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  codeport
+//
+//  Created by Uli Kusterer on 13.05.17.
+//  Copyright © 2017 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
